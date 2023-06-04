dae86ae2e8d12084e3cebe65d1d2b05b92d5af30
SQLAdmin <abuyeahia24@gmail.com>
2023-06-04 11:20:09 +0600

    added navbar and footer basic formate also added banner section

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,31 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { Component } from 'react';
+import { Carousel } from "react-responsive-carousel";
+
+
+
+import img1 from '../../../assets/banner1.png'
+import img2 from '../../../assets/banner2.png'
+import img3 from '../../../assets/banner3.png'
+import img4 from '../../../assets/banner4.png'
+
+const Banner = () => {
+    return (
+        <Carousel autoPlay>
+            <div>
+                <img src={img1} />
+            </div>
+            <div>
+                <img src={img2} />
+            </div>
+            <div>
+                <img src={img3} />
+            </div>
+            <div>
+                <img src={img4} />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
