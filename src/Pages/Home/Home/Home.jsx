92b6dccc2f230a31586d430f8243f7281d7cd968
SQLAdmin <abuyeahia24@gmail.com>
2023-06-04 17:03:44 +0600

    added testimonial part

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Blog from "../Blog/Blog";
 import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
+import Testimonials from "../Testimonial/Testimonials";
 
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <PopularMenu></PopularMenu>
             <Featured></Featured>
             <Blog></Blog>
+            <Testimonials></Testimonials>
         </div>
     );
 };
---
+++ src/Pages/Home/Testimonial/Testimonials.jsx
@@ -0,0 +1,51 @@
+import SectionTitle from "../../../components/sectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import '@smastrom/react-rating/style.css'
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    return (
+        <section className="my-20">
+            <SectionTitle
+                subHeading='What Our Client Said'
+                heading='Testimonials'
+            >
+
+            </SectionTitle>
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review._id}
+                    >
+                        <div className="flex flex-col items-center mx-24 my-16">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <img style={{borderRadius: '50px 50px 50px 50px', width: '120px'}} src={review.img} alt="" />
+                            <h3 className="text-2xl text-green-500 py-8">{review.name}</h3>
+                            <p>{review.details}</p>
+                        </div>
+
+                    </SwiperSlide>)
+                }
+            </Swiper>
+        </section>
+    );
+};
+
+export default Testimonials;
